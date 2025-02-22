ff687ea0e078b1800714f7f6a33fcd1192c3bc65
MathItYT <benjamin.ubilla@uc.cl>
2025-02-22 16:21:55 -0300

    Bump @mathlikeanim-rs/renderer with the new @mathlikeanim-rs/mathlikeanim-rs's version

--- examples/hello-world/main.js
+++ examples/hello-world/main.js
@@ -3,6 +3,7 @@ import { CanvasScene } from '@mathlikeanim-rs/renderer';
 
 const scene = new CanvasScene(1920, 1080, 60);
 let firstTime = true;
+let fontFace;
 
 const run = async () => {
     if (firstTime) {
@@ -11,21 +12,14 @@ const run = async () => {
         canvas.style.width = '50vw';
         canvas.style.height = 'auto';
         document.body.appendChild(canvas);
+        fontFace = new Uint8Array(await fetch('latinmodern-math.otf').then(response => response.blob()).then(blob => blob.arrayBuffer()));
     }
     scene.objects.forEach(obj => obj.free());
     scene.objects = [];
-    const fonts = await window.queryLocalFonts({
-        postscriptNames: ['LatinModernMath-Regular'],
-    });
-    const fontArrays = [];
-    for (const fontData of fonts) {
-        const fontArray = new Uint8Array(await new Response(await fontData.blob()).arrayBuffer());
-        fontArrays.push(fontArray);
-    }
     const obj = new Typst(`#show math.equation: set text(font: "Latin Modern Math")
         #set page(fill: none)
         #let name = "Typst"
-        Hola`).render_to_vector_object_builder(fontArrays.map(fontArray => new FontFace(fontArray)))
+        Hola`).render_to_vector_object_builder([new FontFace(fontFace)])
                 .scale_to_width(1000)
                 .move_to(new Point2D(960, 540))
                 .set_name('Typst')
@@ -38,7 +32,6 @@ const run = async () => {
         }
         const drawPathT = new IntegerLerp(0, 2, t);
         const strokeWidth = 0.25;
-        drawPathT.index === 0 ? console.log(drawPathT.remainder) : null;
         return new VectorObjectBuilder(old)
             .pointwise_become_partial(0, drawPathT.index === 0 ? drawPathT.remainder : 1)
             .set_stroke(Style.from_color(new Color(0, 0, 0, 1)))
@@ -46,11 +39,7 @@ const run = async () => {
             .fade_fill(drawPathT.index === 0 ? 1 : 1 - drawPathT.remainder)
             .build();
     });
-    await scene.play(
-        animations,
-        5000,
-        t => t,
-    );
+    await scene.play(animations, 2000, t => t);
 };
 
 const renderButton = document.getElementById('render-button');
