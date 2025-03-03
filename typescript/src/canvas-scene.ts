631cac3d62ed4461535482b714db1be1780eb56a
MathItYT <benjamin.ubilla@uc.cl>
2025-03-02 21:20:35 -0300

    make worker script endpoint optional

--- typescript/src/canvas-scene.ts
+++ typescript/src/canvas-scene.ts
@@ -20,14 +20,15 @@ export default class CanvasScene extends SVGScene {
      * Creates a new CanvasScene.
      * @param {number} width - The width of the canvas.
      * @param {number} height - The height of the canvas.
+     * @param {string} [workerFileName] - The endpoint of the worker script.
      */
-    constructor(width: number, height: number, workerFileName: string) {
+    constructor(width: number, height: number, workerFileName?: string) {
         super(width, height);
         this.canvas = document.createElement("canvas");
         this.canvas.width = width;
         this.canvas.height = height;
         const offscreen = this.canvas.transferControlToOffscreen();
-        this.worker = new Worker(workerFileName);
+        this.worker = new Worker(workerFileName || '/node_modules/@mathlikeanim-rs/renderer/dist/offscreen-canvas-worker.js');
         this.worker.postMessage(offscreen, [offscreen]);
     }
 
